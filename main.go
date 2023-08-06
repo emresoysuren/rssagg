536eecbe9877975b7b6ba29e2cae756d9f790182
emresoysuren <emresoysuren03@gmail.com>
2023-08-06 14:45:18 +0300

    added rss scraper feat

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/emresoysuren/rssagg/internal/database"
 	"github.com/go-chi/chi"
@@ -36,9 +37,12 @@ func main() {
 		log.Fatalln("Can't connect to database:", err)
 	}
 
+	db := database.New(conn)
 	apiCfg := apiConfig{
-		DB: database.New(conn),
+		DB: db,
 	}
+
+	go startScraping(db, 10, 1*time.Minute)
 
 	router := chi.NewRouter()
 
